ff31118dc69fe04c69d4cb1ee474d1edab830365
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2017-05-30 07:26:19 +0200

    RoboKart: new multi user/multi robo game

---
+++ robo-hub-android/RoboKart/app/src/main/java/de/hu_berlin/informatik/robokart/RoboKart.java
@@ -0,0 +1,13 @@
+package de.hu_berlin.informatik.robokart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoboKart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_robo_kart_view);
+    }
+}
